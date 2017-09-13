4531f76f5870fe769f8c7b5542205ea6f29fb5d2
msimw <msimw@sina.com>
2017-09-13 11:04:32 +0800

    fix

---
+++ src/test/java/com/msimw/ABTest.java
@@ -0,0 +1,18 @@
+package com.msimw;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by msimw on 17-9-12.
+ */
+@Ignore
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-httpclient-retrofit2.xml")
+public class ABTest {
+
+
+
+}
